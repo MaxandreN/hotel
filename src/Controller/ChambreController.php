dea74ec0a6f320b39a7bfe5c526707d13090f2f7
MaxandreN <Maxandre.neveux@gmail.com>
2020-11-16 22:23:55 +0100

    Save comit befor authent

--- src/Controller/ChambreController.php
+++ src/Controller/ChambreController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Chambre;
+use App\Entity\Statut;
+use App\Entity\Tache;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ChambreController extends AbstractController
 {
     /**
-     * @Route("/chambre", name="chambre")
+     * @Route("/chambre", name="chambre_index")
      */
     public function index(): Response
     {
@@ -19,10 +21,11 @@ class ChambreController extends AbstractController
 
         $chambres = $repo->findAll();
 
+
         return $this->render('chambre/index.html.twig', [
             'controller_name' => 'ChambreController',
             'role_user' => 'manager',
-            // 'chambres' => $chambres
+            'chambres' => $chambres
         ]);
 
     }
@@ -35,6 +38,23 @@ class ChambreController extends AbstractController
             'controller_name' => 'ChambreController',
             'role_user' => 'manager'
         ]);
+    }
+
+        /**
+     * @Route("/chambre/edit/statut/{id_chambre}/{id_statut}", name="edit_statut_chambre")
+     */
+    public function editChambreStatut($id_chambre, $id_statut){
+        $entityManager = $this->getDoctrine()->getManager();
+        $repo = $entityManager->getRepository(Chambre::class);
+        $repo2 = $entityManager->getRepository(Statut::class);
+        $statut = $repo2->find($id_statut);
+
+        $chambre = $repo->find($id_chambre);
+        $chambre->setStatut($statut);
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('chambre_index');
     }
 }
   
